e45c54a3fa983c109e1be523b2f3266f734f2242
Sergio Peris <dev.sertxu@gmail.com>
2019-03-25 00:36:42 +0100

    Modified footer copyright translation

--- publishable/lang/en/theme.php
+++ publishable/lang/en/theme.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-  "footer_copyright" => "Copyright © 2018 <a href=\"//sertxudeveloper.com\">Sertxu Developer</a>",
+  "footer_copyright" => "Copyright © 2018 - :year <a href=\"//sertxudeveloper.com\">Sertxu Developer</a>",
   "version" => "Version",
   "terms_use" => "Term of use",
   "privacy_policy" => "Privacy policy"
