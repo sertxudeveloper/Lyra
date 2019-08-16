a1d945237c12a00579f3defa81387df21f982a07
Sergio Peris <dev.sertxu@gmail.com>
2019-08-16 23:49:18 +0200

    Added Belongs To Many field read

---
+++ resources/assets/js/components/Fields/Read/BelongsToManyField.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span class="badge badge-primary mr-1" v-for="element in field.value">{{element.value}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['field'],
+  }
+</script>
+
+<style scoped>
+  .badge {
+    font-size: 85%;
+  }
+</style>
