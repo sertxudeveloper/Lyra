105cffa2e3ae6f9c9ba53928f7e2155dd3d8b6fc
Sergio Peris <dev.sertxu@gmail.com>
2019-08-16 23:18:53 +0200

    Modified language files due to inconsistency in the footer text

--- publishable/lang/es/theme.php
+++ publishable/lang/es/theme.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-  "footer_copyright" => "Copyright © 2018 <a href=\"//sertxudeveloper.com\">Sertxu Developer</a>",
+  "footer_copyright" => "Copyright © 2018 - :year <a href=\"//sertxudeveloper.com\">Sertxu Developer</a>",
   "version" => "Versión",
   "terms_use" => "Terminos de uso",
   "privacy_policy" => "Política de privacidad"
