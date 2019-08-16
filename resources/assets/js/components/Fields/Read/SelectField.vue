56125f59b642da2b1b9164403a6424a6437a0166
Sergio Peris <dev.sertxu@gmail.com>
2019-08-16 23:48:09 +0200

    Added Select Field

---
+++ resources/assets/js/components/Fields/Read/SelectField.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>{{field.value}}</span>
+</template>
+
+<script>
+  export default {
+    props: ['field']
+  }
+</script>
+
+<style scoped>
+  .fa-times-circle {
+    color: #bd2130;
+  }
+
+  .fa-check-circle {
+    color: #218838;
+  }
+</style>
