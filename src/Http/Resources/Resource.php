c4198e2b6fa6fa39dc66dd8a7e18fbe404f8a872
Sergio Peris <dev.sertxu@gmail.com>
2019-09-02 17:50:15 +0200

    Now the base resource works with translations

--- src/Http/Resources/Resource.php
+++ src/Http/Resources/Resource.php
@@ -15,6 +15,8 @@ abstract class Resource extends ResourceCollection {
   public $singular;
   public $plural;
 
+  protected $response = [];
+
   public abstract function fields();
 
   public static function getPrimary() {
@@ -25,51 +27,60 @@ abstract class Resource extends ResourceCollection {
     return collect((new static($resource))->fields());
   }
 
+  protected function getAvailableLanguages() {
+    $available_locales = config('lyra.translator.available_locales');
+    $default_locale = config('lyra.translator.default_locale');
+    array_unshift($available_locales, $default_locale);
+    return $available_locales;
+  }
+
   public function getCollection(Request $request, string $type) {
     $this->type = $type;
 
     $singular = ($this->singular) ? $this->singular : Str::singular(class_basename($this));
     $plural = ($this->plural) ? $this->plural : Str::plural(class_basename($this));
 
-    return [
-      "labels" => [
-        "singular" => $singular,
-        "plural" => $plural
-      ],
-      "collection" => $this->toArray($request)
-    ];
+    $this->response['labels'] = ["singular" => $singular, "plural" => $plural];
+    if (config('lyra.translator.enabled')) $this->response['languages'] = $this->getAvailableLanguages();
+    $this->response['collection'] = $this->toArray($request);
+
+    return $this->response;
   }
 
   public function toArray($request) {
     $resource = ($this->type !== 'create') ? $this->resource->toArray() : [];
-    $this->collection = ($this->type !== 'create') ? $this->collection : collect([[]]);
-
+    $this->collection = ($this->type !== 'create') ? $this->collection : collect([new static::$model]);
 
     if (!Arr::first($resource) || $this->type !== 'index') $resource = [];
     $resource = (object)$resource;
 
     $resource->data = $this->collection->map(function ($item) use ($request) {
-      if (isset($item->load)) $item->load($this->with);
       $fields = [];
 
       foreach ($this->fields() as $field) {
         $permission = $field->getPermissions();
         if ($permission['hideOn' . ucfirst($this->type)]) continue;
 
-        $field = $field->getValue($item, $this->type);
-
+        $field = $field->getValue($item, $this->type, get_class($this));
         $fields[] = $field;
       }
-
       return $fields;
     });
 
-    $resource->hasSoftDeletes = in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses(static::$model));
+    $resource->hasSoftDeletes = $this->hasSoftDeletes();
 
     $resource = collect($resource)->filter(function ($item, $key) {
       return !preg_match('/^[0-9]$/', $key);
     })->toArray();
 
     return $resource;
+  }
+
+  public static function hasSoftDeletes() {
+    return in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses(static::$model));
+  }
+
+  public static function isTranslatable() {
+    return in_array('App\Traits\Common\Translatable', class_uses(static::$model));
   }
 }
