a3a7534b331961ca47f73a11d0da85decaec0ef5
Sergio Peris <dev.sertxu@gmail.com>
2019-09-02 17:49:45 +0200

    Created the Translator controller

---
+++ src/Http/Controllers/TranslatorController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SertxuDeveloper\Lyra\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use SertxuDeveloper\Lyra\Lyra;
+
+class TranslatorController extends Controller {
+
+  public static function removeTranslation($lang, $element) {
+    DB::table($element->getTable() . '_translations')
+      ->where([['locale', $lang], ['id', $element[$element->getKeyName()]]])->delete();
+  }
+
+  public static function removeTranslations($element) {
+    DB::table($element->getTable() . '_translations')
+      ->where('id', $element[$element->getKeyName()])->delete();
+  }
+
+  public static function isTranslatorUsable() {
+    return config('lyra.translator.enabled') && request()->get('lang') &&
+      request()->get('lang') !== config('lyra.translator.default_locale');
+  }
+
+  public static function updateTranslation($values, $resource) {
+    $fields = $values->filter(function ($value) {
+      return isset($value['translatable']);
+    })->mapWithKeys(function ($field) {
+      return [$field['column'] => $field['value']];
+    })->toArray();
+
+    $translation = DB::table($resource->getTable() . '_translations')
+      ->where([['locale', request()->get('lang')], ['id', $resource[$resource->getKeyName()]]])->first();
+
+    if ($translation) {
+
+      $data = ['updated_at' => new Carbon()];
+      $data = array_merge($data, $fields);
+
+      DB::table($resource->getTable() . '_translations')
+        ->where([['locale', request()->get('lang')], ['id', $resource[$resource->getKeyName()]]])
+        ->update($data);
+    } else {
+
+      $data = [
+        'id' => $resource[$resource->getKeyName()],
+        'locale' => request()->get('lang'),
+        'created_at' => new Carbon(),
+        'updated_at' => new Carbon()
+      ];
+
+      $data = array_merge($data, $fields);
+      DB::table($resource->getTable() . '_translations')->insert($data);
+    }
+  }
+
+}
