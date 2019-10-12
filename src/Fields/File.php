79f2759b783126ee7e86c8ac070e0375e3661a30
Sergio Peris <dev.sertxu@gmail.com>
2019-10-12 23:23:00 +0200

    Solved problem with the files and images

--- src/Fields/File.php
+++ src/Fields/File.php
@@ -7,88 +7,105 @@ use Illuminate\Support\Facades\Storage;
 class File extends Field {
 
   protected $component = "file-field";
-
-  protected $hideOnIndex = true;
-  protected $multiple = false;
-  protected $disk = 'public';
-  protected $prunable = false;
-  protected $folder = false;
-  protected $originalName = false;
+  protected $defaultDisk = 'public';
 
   public function multiple() {
-    $this->multiple = true;
+    $this->data->put('multiple', true);
     return $this;
   }
 
   public function disk($disk) {
-    $this->disk = $disk;
+    $this->data->put('disk', $disk);
     return $this;
   }
 
   public function prunable() {
-    $this->prunable = true;
+    $this->data->put('prunable', true);
     return $this;
   }
 
   public function folder($folder) {
-    $this->folder = $folder;
+    $this->data->put('folder', $folder);
     return $this;
   }
 
   public function originalName() {
-    $this->originalName = true;
+    $this->data->put('originalName', true);
     return $this;
   }
 
   public function get() {
+    if (!$this->data->get('disk')) $this->data->put('disk', $this->defaultDisk);
+    $this->data->put('storage_path', Storage::disk($this->data->get('disk'))->url(null));
+    return $this->data->toArray();
+  }
 
-    return [
-      "component" => $this->component,
-      "name" => $this->name,
-      "column" => $this->column,
-      "description" => $this->description,
-      "sortable" => $this->sortable,
-      "primary" => $this->primary,
-      "multiple" => $this->multiple,
-      "storage_path" => Storage::disk($this->disk)->url(null),
-      "value" => ($this->multiple) ? json_decode($this->value) : $this->value,
-    ];
+  protected function retrieveValue($model) {
+    if (!isset($model[$this->data->get('column')])) return null;
+
+    /** Decode the value if required */
+    if ($this->data->get('multiple')) {
+      return json_decode($model[$this->data->get('column')]);
+    } else {
+      return $model[$this->data->get('column')];
+    }
   }
 
   public function saveValue($field, $resource) {
-    if ($this->multiple) {
-      $old = json_decode($resource[$this->column]);
+    /** Check if there is a disk defined, if not set the default disk */
+    if (!$this->data->get('disk')) $this->data->put('disk', $this->defaultDisk);
+
+    /** Check if the field can handle multiple files and get the old paths saved in the database */
+    if ($this->data->get('multiple')) {
+      $old = json_decode($resource[$this->data->get('column')]);
       if (!$old) $old = [];
     } else {
-      $old = $resource[$this->column];
+      $old = $resource[$this->data->get('column')];
     }
 
-    if ($this->prunable) {
+    /** Check if the field is prunable */
+    if ($this->data->get('prunable')) {
+      /** Get the old files not longer used and remove it from the disk */
       collect($old)->diff($field['value'])->each(function ($file) {
-        Storage::disk($this->disk)->delete($file);
+        Storage::disk($this->data->get('disk'))->delete($file);
       });
     }
 
-    $folder = (!$this->folder) ? $this->column : $this->folder;
+    /** If there's not a folder name defined use the column name */
+    $folder = (!$this->data->get('folder')) ? $this->data->get('column') : $this->data->get('folder');
+
+    /** Check if the folder exists, if not create it */
+    if (!Storage::disk($this->data->get('disk'))->exists($folder)) Storage::disk($this->data->get('disk'))->makeDirectory($folder);
+
+    /** Get the array with the files paths */
     $paths = collect($field['value'])->intersect($old);
 
-    $column = $this->column;
+    /** Get the file keys as "file-1", "file-2", "file-X" because that are the new files */
+    $column = $this->data->get('column');
     $filesKeys = collect(request()->files->keys())->filter(function ($value) use ($column) {
       $explode = explode('-', $value);
       array_pop($explode);
       return implode('-', $explode) === $column;
     });
 
+    /** Iterate the file keys getted above */
     foreach ($filesKeys as $fileKey) {
-      if ($this->originalName) {
-        $path = request()->file($fileKey)->storeAs($folder, request()->file($fileKey)->getClientOriginalName(), $this->disk);
+      /** Check if the file should maintain the original name */
+      if ($this->data->get('originalName')) {
+        /** Save the file with the original name and get the path */
+        $path = request()->file($fileKey)->storeAs($folder, request()->file($fileKey)->getClientOriginalName(), $this->data->get('disk'));
       } else {
-        $path = request()->file($fileKey)->store($folder, $this->disk);
+        /** Save the file with a new name and get the path */
+        $path = request()->file($fileKey)->store($folder, $this->data->get('disk'));
       }
+      /** Add the new path to the paths collection */
       $paths->push($path);
     }
 
+    /** Get all the paths values as an array */
     $paths = $paths->values();
-    $resource->{$this->column} = ($this->multiple) ? json_encode($paths->toArray()) : $paths->first() ;
+
+    /** Save the path in the resource column, if the field handle multiple files save the paths as an encoded json */
+    $resource->{$this->data->get('column')} = ($this->data->get('multiple')) ? json_encode($paths->toArray()) : $paths->first();
   }
 }
--- src/Fields/Image.php
+++ src/Fields/Image.php
@@ -2,31 +2,8 @@
 
 namespace SertxuDeveloper\Lyra\Fields;
 
-use Illuminate\Support\Facades\Storage;
-
 class Image extends File {
 
   protected $component = "image-field";
 
-//  public function updateValue($field, $old) {
-//    if ($this->prunable) Storage::disk($this->disk)->delete($old);
-//
-//    $folder = (!$this->folder) ? $this->column : $this->folder;
-//    $paths = [];
-//
-//    $column = $this->column;
-//    $filesKeys = collect(request()->files->keys())->filter(function ($value) use ($column) {
-//      $explode = explode('-', $value);
-//      array_pop($explode);
-//      return implode('-', $explode) === $column;
-//    });
-//
-//    foreach ($filesKeys as $fileKey) {
-//      $path = request()->file($fileKey)->store($folder, $this->disk);
-//      array_push($paths, $path);
-//    }
-//
-//    if(count($paths) === 0) return null;
-//    return count($paths) > 1 ? json_encode($paths) : $paths[0];
-//  }
 }
