f2ff4ad8c5d3c2213f22f56fe0730b97a9325f0e
Sergio Peris <dev.sertxu@gmail.com>
2019-10-12 23:22:16 +0200

    Removed old comments

--- src/Fields/Id.php
+++ src/Fields/Id.php
@@ -5,14 +5,6 @@ namespace SertxuDeveloper\Lyra\Fields;
 class Id extends Field {
 
   protected $component = "id-field";
-
-//  protected $sortable = true;
-//  protected $primary = true;
-
   protected $hideOnCreate = true;
-//  protected $hideOnEdit = true;
 
-//  protected static function getNewInstance() {
-//    return new self();
-//  }
 }
