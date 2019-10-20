52c09c3a758c0c31079bf867d04656dfc16159f9
Sertxu Developer <dev.sertxu@gmail.com>
2019-10-21 01:12:11 +0200

    Updated the Select field to the new format

--- src/Fields/Select.php
+++ src/Fields/Select.php
@@ -5,24 +5,10 @@ namespace SertxuDeveloper\Lyra\Fields;
 class Select extends Field {
 
   protected $component = "select-field";
-//  protected $options = ["ACTIVE", "BLOCKED"];
 
   public function options(array $options) {
-    $this->options = $options;
+    $this->data->put('options', $options);
     return $this;
   }
 
-  public function get() {
-
-    return [
-      "component" => $this->component,
-      "name" => $this->name,
-      "column" => $this->column,
-      "description" => $this->description,
-      "sortable" => $this->sortable,
-      "primary" => $this->primary,
-      "options" => $this->options,
-      "value" => $this->value,
-    ];
-  }
 }
