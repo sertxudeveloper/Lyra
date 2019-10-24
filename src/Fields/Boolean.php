ce74d11d39fe6d578c5fc2dd1e651f5fdad77aa9
Sertxu Developer <dev.sertxu@gmail.com>
2019-10-24 10:14:42 +0200

    Boolean field now can handle customized values and a default checked functionality

--- src/Fields/Boolean.php
+++ src/Fields/Boolean.php
@@ -6,16 +6,30 @@ class Boolean extends Field {
 
   protected $component = "boolean-field";
 
+  protected $trueValue;
+  protected $falseValue;
+  protected $checked = false;
+
+  public function setValues($true, $false) {
+    $this->trueValue = $true;
+    $this->falseValue = $false;
+    return $this;
+  }
+
+  public function checked() {
+    $this->checked = true;
+    return $this;
+  }
+
   protected function retrieveValue($model) {
-    if(isset($model[$this->data->get('column')])) {
-      return ($model[$this->data->get('column')] == 'ACTIVE') ? true : false;
-    } else {
-      return false;
+    if (isset($model[$this->data->get('column')])) {
+      return ($model[$this->data->get('column')] == $this->trueValue) ? true : false;
     }
+    return $this->checked;
   }
 
   public function saveValue($field, $resource) {
-    $resource[$this->data->get('column')] = $field['value'] ? 'ACTIVE' : 'BLOCKED';
+    $resource[$this->data->get('column')] = $field['value'] ? $this->trueValue : $this->falseValue;
   }
 
 }
