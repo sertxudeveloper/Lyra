6dd30ba38da54a09c51bc97a80937530ab6ff489
Sertxu Developer <dev.sertxu@gmail.com>
2019-10-25 19:47:58 +0200

    Overrided static method make

--- src/Fields/Id.php
+++ src/Fields/Id.php
@@ -2,9 +2,33 @@
 
 namespace SertxuDeveloper\Lyra\Fields;
 
+use Illuminate\Support\Str;
+
 class Id extends Field {
 
   protected $component = "id-field";
   protected $hideOnCreate = true;
+
+  public static function make($name, $column = null) {
+    $class = new static();
+    $class->data = collect([]);
+
+    if (!$column) {
+      $column = Str::snake($name);
+    } else {
+      if (is_callable($column)) {
+        $class->callback = $column;
+        $column = Str::snake($name);
+        $class->hideOnCreate = true;
+        $class->hideOnEdit = true;
+      }
+    }
+
+    $class->data->put('component', $class->component);
+    $class->data->put('name', $name);
+    $class->data->put('column', $column);
+    $class->data->put('primary', true);
+    return $class;
+  }
 
 }
