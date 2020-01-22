7061a76746cd6f01c014fb4c72d039f4ff5818b6
Sertxu Developer <dev.sertxu@gmail.com>
2020-01-22 13:32:08 +0100

    Created textarea fields

---
+++ resources/assets/js/components/Fields/Edit/TextareaField.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="text-right">
+    <textarea class="form-control" :title="field.name" :id="field.column" v-model="field.value" cols="30" rows="10"
+              :maxlength="(field.size && field.size.hardMode) ? field.size.number : false"></textarea>
+    <small v-if="field.size" class="font-weight-bold text-muted">
+      <span :class="classLength">{{ length }}</span> / {{ field.size.number }}
+    </small>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['field', 'formData'],
+    computed: {
+      length: function () {
+        return (this.field.value) ? this.field.value.length : 0;
+      },
+      classLength: function () {
+        let classLength = '';
+
+        if ((this.field.size.number - this.length) > 10) {
+          classLength = 'text-success';
+        } else if ((this.field.size.number - this.length) <= 10 && (this.field.size.number - this.length) >= 0) {
+          classLength = 'text-warning';
+        } else {
+          classLength = 'text-danger';
+        }
+
+        return classLength;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Fields/Textarea.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SertxuDeveloper\Lyra\Fields;
+
+class Textarea extends Text {
+
+  protected $component = "textarea-field";
+
+}
