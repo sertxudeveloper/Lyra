eef28fa74f69d2ea7e3a14f045cd7cb87eb7021e
Sertxu Developer <dev.sertxu@gmail.com>
2020-03-07 00:20:21 +0100

    Added retrieveValue to the Date field

--- src/Fields/Date.php
+++ src/Fields/Date.php
@@ -6,4 +6,7 @@ class Date extends Field {
 
   protected $component = "date-field";
 
+  protected function retrieveValue($model) {
+    return isset($model[$this->data->get('column')]) ? $model[$this->data->get('column')]->format('Y-m-d') : null;
+  }
 }
