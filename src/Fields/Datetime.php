c019ac92d644b57900eedd9953466f4a6a533fd2
Sertxu Developer <dev.sertxu@gmail.com>
2020-03-07 00:36:14 +0100

    Solved problem saving datetime

--- src/Fields/Datetime.php
+++ src/Fields/Datetime.php
@@ -10,4 +10,8 @@ class Datetime extends Field {
     return isset($model[$this->data->get('column')]) ? $model[$this->data->get('column')]->format('Y-m-d\TH:i:s') : null;
   }
 
+  public function saveValue(array $field, $model) {
+    $model[$this->data->get('column')] = new \DateTime($field['value']);
+  }
+
 }
