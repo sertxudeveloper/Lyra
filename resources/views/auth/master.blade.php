5577de0252195d603b67b817992b391e387d67d4
Sertxu Developer <dev.sertxu@gmail.com>
2020-03-22 20:23:24 +0100

    Added favicon to the login view

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -4,6 +4,7 @@
   <title>@yield('title')</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}"/>
+  <link rel="icon" type="image/png" href="{{ lyra_asset('images/favicon.png') }}">
   <link rel="stylesheet" href="{{ lyra_asset('css/login.css') }}">
   <link rel="stylesheet" href="{{ Lyra::getPreferredTheme() }}">
 </head>
