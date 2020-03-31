2963c97f92768bd430a27ddc7fb2f9faabeb32a8
Sertxu Developer <dev.sertxu@gmail.com>
2020-04-01 00:32:10 +0200

    Added 404 error message when the dashboard class is not defined

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -9,6 +9,7 @@ class DashboardController extends Controller {
 
   public function index(Request $request) {
     $dashboardClass = config('lyra.dashboard');
+    if (!$dashboardClass) abort(404, "Dashboard not available!<br>Configure your own in the configuration file.");
     $dashboard = new $dashboardClass();
     return $dashboard->toArray($request);
   }
