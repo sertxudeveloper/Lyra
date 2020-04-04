cd7facbcb4a2136b7759b1f2efad105c55fdb685
Sertxu Developer <dev.sertxu@gmail.com>
2020-04-04 21:01:55 +0200

    Solved problem with nested elements in the menu

--- resources/views/common/sidebar/submenu.blade.php
+++ resources/views/common/sidebar/submenu.blade.php
@@ -8,15 +8,6 @@
   </a>
   <ul class="list-unstyled collapse ml-3" id="{{$item['key']}}Submenu">
     @foreach($item['items'] as $item)
-      {{--<li class="nav-item">
-        <router-link
-          class="nav-link" :to="'/{{$item['key']}}'" exact>
-          <div class="icon">
-            <i class="{{$item['icon']}}"></i>
-          </div>
-          <span class="d-none d-sm-block">{{$item['name']}}</span>
-        </router-link>
-      </li>--}}
       @include('lyra::common.sidebar.item')
     @endforeach
   </ul>
--- src/Http/Controllers/MenuController.php
+++ src/Http/Controllers/MenuController.php
@@ -2,62 +2,61 @@
 
 namespace SertxuDeveloper\Lyra\Http\Controllers;
 
-use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use SertxuDeveloper\Lyra\Lyra;
 
 class MenuController extends Controller {
 
   /**
    * Get the menu collection
-   * @return Collection
+   *
+   * @return array
    */
-  public function getMenu(): Collection {
+  public function getMenu(): array {
+    return $this->buildMenu(config('lyra.menu'));
+  }
 
-    $items = collect(config('lyra.menu'));
-    $menu = collect([]);
-    $items->filter(function (&$item) use ($menu) {
+  /**
+   * @param $items
+   * @return array
+   */
+  private function buildMenu($items) {
+    $menu = [];
 
-      if (isset($item['hidden']) && $item['hidden'] === true) return false;
+    foreach ($items as $item) {
+      if (isset($item['hidden']) && $item['hidden'] === true) continue;
+
+      if (!isset($item['key'])) $item['key'] = Str::snake($item['name']);
+
+      if (isset($item['resource'])) {
+        unset($item['resource']);
+        $item['prefix'] = '/resources';
+      } else if (isset($item['component'])) {
+        $item['prefix'] = '/components';
+      } else {
+        $item['prefix'] = '';
+      }
 
       if (isset($item['items'])) {
-
-        $filtered = collect($item['items'])->filter(function (&$item) {
-          if (isset($item['hidden']) && $item['hidden'] === true) return false;
-          return $this->can($item);
-        });
-
-        $item['items'] = $filtered->toArray();
-
-        if (!$filtered->count()) return false;
-
-        $menu->push($item);
-        return true;
+        $item['items'] = $this->buildMenu($item['items']);
+        if (count($item['items'])) $menu[] = $item;
       } else {
-
-        if (isset($item['resource'])) {
-          $item['prefix'] = '/resources';
-        } else if (isset($item['component'])) {
-          $item['prefix'] = '/components';
-        } else {
-          $item['prefix'] = '';
-        }
-
-        if ($this->can($item)) $menu->push($item);
-        return true;
+        if ($this->can($item)) $menu[] = $item;
       }
-    });
+
+    }
 
     return $menu;
   }
 
   /**
-   * Check if user can access to the $item
+   * Check if user can access the item
+   *
    * @param array $item
    * @return bool
    */
   private function can(array $item): bool {
-    if (config('lyra.authenticator') === 'basic') return true;
+    if (config('lyra.authenticator') === Lyra::MODE_BASIC) return true;
     return Lyra::auth()->user()->hasPermission('read', $item['key']);
   }
-
 }
