d2c61605b461dc96443ef1112ce2614072731a4c
Sertxu Developer <dev.sertxu@gmail.com>
2020-04-04 21:34:49 +0200

    Added seconds to the DateTime html field

--- resources/assets/js/components/Fields/Edit/DatetimeField.vue
+++ resources/assets/js/components/Fields/Edit/DatetimeField.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-right">
-    <input type="datetime-local" v-model="field.value" :title="field.name" class="form-control">
+    <input type="datetime-local" v-model="field.value" :title="field.name" class="form-control" step="1">
   </div>
 </template>
 
