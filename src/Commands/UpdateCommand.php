c1fda519950b3e16a77f83d59b3d6d3dc7328f78
Sertxu Developer <dev.sertxu@gmail.com>
2020-04-17 20:55:06 +0200

    Modified update sequence, now the update command is only available in the advanced mode

--- src/Commands/UpdateCommand.php
+++ src/Commands/UpdateCommand.php
@@ -28,13 +28,15 @@ class UpdateCommand extends Command {
    */
   public function handle() {
     $this->info("Welcome to the Lyra package updater.");
-    $this->call("vendor:publish", ["--provider" => LyraServiceProvider::class, "--tag" => "lyra-assets", "--force" => true]);
 
-    if (config('lyra.authenticator') === Lyra::MODE_ADVANCED) {
-      $this->info("Advanced mode detected, updating the migrations...");
-      $this->call("vendor:publish", ["--provider" => LyraServiceProvider::class, "--tag" => "lyra-migrations", "--force" => true]);
-      $this->call('migrate', ['--path' => 'database/migrations/lyra']);
+    if (config('lyra.authenticator') !== Lyra::MODE_ADVANCED) {
+      $this->error('This command is only available using the Advanced mode!');
+      exit(1);
     }
+
+    $this->info("Advanced mode detected, updating the migrations...");
+    $this->call("vendor:publish", ["--provider" => LyraServiceProvider::class, "--tag" => "lyra-migrations", "--force" => true]);
+    $this->call('migrate', ['--path' => 'database/migrations/lyra']);
 
     $this->info('Successfully updated! Enjoy creating awesome things with Lyra.');
   }
