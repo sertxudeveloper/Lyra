717e1a49c421bf97a6b633729d1d011ffa26c82e
Sertxu Developer <dev.sertxu@gmail.com>
2020-04-17 20:51:10 +0200

    Modified installation sequence

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -9,8 +9,6 @@ use SertxuDeveloper\Lyra\Traits\Seedable;
 class InstallCommand extends Command {
   use Seedable;
 
-  protected $seedersPath = __DIR__ . '/../../publishable/database/seeds/';
-
   /**
    * The console command name.
    *
@@ -32,29 +30,30 @@ class InstallCommand extends Command {
   public function handle() {
     $this->info("Welcome to the Lyra package installation. Let's start!");
 
-    $this->info("First we're going to publish the assets files");
-    $this->call("vendor:publish", ["--provider" => LyraServiceProvider::class, "--tag" => "lyra-assets"]);
-
-    $this->info("Now, we're going to publish the config file");
+    $this->info("First, we're going to publish the config file");
     $this->call("vendor:publish", ["--provider" => LyraServiceProvider::class, "--tag" => "lyra-config"]);
 
     if ($this->confirm("Are you going to use the advanced working mode?")) {
+      $this->replace('Lyra::MODE_BASIC', 'Lyra::MODE_ADVANCED', config_path('lyra.php'));
+      $this->replace('LaravelUser::class','LyraUser::class', config_path('lyra.php'));
+
       $this->info("After that, we're going to publish the database files and run the migrations");
       $this->call("vendor:publish", ["--provider" => LyraServiceProvider::class, "--tag" => "lyra-migrations"]);
 
       $this->call('migrate', ['--path' => 'database/migrations/lyra']);
     }
 
-    $this->info("Finally is your turn, I need you to decide if you want to publish the optional files o not");
-
-    if ($this->confirm('Do you want to publish the views files?')) {
-      $this->call("vendor:publish", ["--provider" => LyraServiceProvider::class, "--tag" => "lyra-views"]);
-    }
-
-    if ($this->confirm('Do you want to add the storage symlink to the public folder?')) {
-      $this->call('storage:link');
-    }
-
     $this->info('Successfully installed! Enjoy creating awesome things with Lyra.');
+  }
+
+  /**
+   * Replace the $search with $replace in the $path file.
+   *
+   * @param $search
+   * @param $replace
+   * @param $path
+   */
+  private function replace($search, $replace, $path) {
+    file_put_contents($path, str_replace($search, $replace, file_get_contents($path)));
   }
 }
