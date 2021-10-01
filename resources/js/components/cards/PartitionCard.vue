ea95036670105841a2304f024d007b543dbee1dd
Sergio Peris <dev.sertxu@gmail.com>
2021-10-01 20:50:20 +0200

    wip

--- resources/js/components/cards/PartitionCard.vue
+++ resources/js/components/cards/PartitionCard.vue
@@ -1,10 +1,30 @@
 <template>
-
+  <div class="bg-white flex flex-col justify-between p-4 rounded-lg shadow">
+    <div class="flex flex-col">
+      <div class="flex items-center justify-between mb-2">
+        <div class="font-semibold text-gray-500 text-sm tracking-wider uppercase">{{ card.label }}</div>
+        <div class="text-gray-400 text-sm tracking-wide">{{ card.total }} total</div>
+      </div>
+      <div class="gap-y-1 gap-x-4 grid grid-cols-2">
+        <div class="flex items-center text-gray-500 text-xs" v-for="group in card.values">
+          <div class="flex flex-shrink-0 h-2.5 mr-1.5 rounded-full w-2.5" :style="`background-color: ${group.color}`"></div>
+          <div class="flex justify-between w-full">
+            <div class="">{{ group.label }}</div>
+            <div>{{ group.value }} - ({{ group.percent.toFixed(2) }}%)</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-2 mt-3 overflow-hidden rounded-full w-full flex">
+      <div class="h-full" v-for="group in card.values" :style="`width: ${group.percent}%; background-color: ${group.color}`"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PartitionCard"
+  name: "PartitionCard",
+  props: ['card'],
 }
 </script>
 
--- src/Cards/PartitionCard.php
+++ src/Cards/PartitionCard.php
@@ -2,9 +2,162 @@
 
 namespace SertxuDeveloper\Lyra\Cards;
 
-class PartitionCard extends Card {
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
+abstract class PartitionCard extends Card {
+
+  /**
+   * The card component
+   *
+   * @var string
+   */
   public string $component = 'card-partition';
 
-  public string $name = '';
+  /**
+   * Precision for the rounding method
+   *
+   * @var int
+   */
+  public int $precision = 0;
+
+  /**
+   * Limit the quantity of results
+   *
+   * @var int
+   */
+  public int $limit = 6;
+
+  /**
+   * Calculate the value in the specified range
+   * Supported 'count', 'min', 'max', 'avg' and 'sum' methods
+   *
+   * @param Request $request
+   * @return array[]
+   */
+  abstract public function calculate(Request $request): array;
+
+  /**
+   * Get the available colors
+   *
+   * @return string[]
+   */
+  public function colors(): array {
+    return [
+      "#F87171",
+      "#FBBF24",
+      "#34D399",
+      "#60A5FA",
+      "#A78BFA",
+      "#F472B6",
+    ];
+  }
+
+  /**
+   * Count instances of the model
+   *
+   * @param Request $request
+   * @param Builder|string $model
+   * @param string $groupBy
+   * @param string|null $column
+   * @return array[]
+   */
+  public function count(Request $request, $model, string $groupBy, ?string $column = null): array {
+    return $this->query($request, $model, 'count', $column, $groupBy);
+  }
+
+  /**
+   * Get the maximum value of the specified column
+   *
+   * @param Request $request
+   * @param Builder|string $model
+   * @param string $column
+   * @param string $groupBy
+   * @return array[]
+   */
+  public function max(Request $request, $model, string $column, string $groupBy): array {
+    return $this->query($request, $model, 'max', $column, $groupBy);
+  }
+
+  /**
+   * Get the minimum value of the specified column
+   *
+   * @param Request $request
+   * @param Builder|string $model
+   * @param string $column
+   * @param string $groupBy
+   * @return array[]
+   */
+  public function min(Request $request, $model, string $column, string $groupBy): array {
+    return $this->query($request, $model, 'min', $column, $groupBy);
+  }
+
+  /**
+   * Return the result of the query
+   *
+   * @param Request $request
+   * @param Builder|string $model
+   * @param string $method
+   * @param string|null $column
+   * @param string $groupBy
+   * @return array[]
+   */
+  public function query(Request $request, $model, string $method, ?string $column, string $groupBy): array {
+    $query = $model instanceof Builder ? $model : $model::query();
+    $column = $column ?? $query->getModel()->getQualifiedKeyName();
+
+    if (!collect($query->getQuery()->groups)->contains('label')) {
+      $query = $query->addSelect(DB::raw("$groupBy as label"))->groupBy($groupBy);
+    }
+
+    $values = $query
+      ->addSelect(DB::raw("$method($column) as value"))
+      ->limit($this->limit)
+      ->orderByDesc('value')
+      ->get();
+
+    $total = collect($values)->sum('value');
+
+    $values->mapWithKeys(function ($item, $key) use ($total) {
+      $item['color'] = $this->colors()[$key];
+      $item['value'] = (float)$item['value'];
+      $item['percent'] = round($item['value'] / $total * 100, 3);
+      return [$key => $item];
+    });
+
+    return $values->toArray();
+  }
+
+  /**
+   * Get the aggregate value of the specified column
+   *
+   * @param Request $request
+   * @param Builder|string $model
+   * @param string $column
+   * @param string $groupBy
+   * @return array[]
+   */
+  public function sum(Request $request, $model, string $column, string $groupBy): array {
+    return $this->query($request, $model, 'sum', $column, $groupBy);
+  }
+
+  /**
+   * Transform the card into a JSON array.
+   *
+   * @param Request $request
+   * @return array
+   */
+  public function toArray(Request $request): array {
+    $values = $this->calculate($request);
+    $total = collect($values)->sum('value');
+
+    return [
+      'component' => $this->component,
+      'label' => $this->label(),
+      'slug' => $this->slug(),
+      'total' => $total,
+      'values' => $values,
+    ];
+  }
 }
