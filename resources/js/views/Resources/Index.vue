abc1a5e7598a3f745b1955b02c7964bfbda9fe00
Sergio Peris <dev.sertxu@gmail.com>
2021-12-27 14:15:34 +0100

    wip

--- resources/js/views/Resources/Index.vue
+++ resources/js/views/Resources/Index.vue
@@ -12,18 +12,19 @@
     <div class="flex items-center justify-between h-9 my-4">
       <!-- Search tool -->
       <div class="bg-white border border-gray-300 flex h-10 items-center rounded-md text-gray-500">
-        <div class="flex-shrink-0 h-4 mx-2 w-4">
+        <div class="flex-shrink-0 h-4 ml-3 mr-2 w-4">
           <svg height="16" width="16" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M19.8633 18.3164L15.1211 13.5742C15.0312 13.4844 14.9141 13.4375 14.7891 13.4375H14.2734C15.5039 12.0117 16.25 10.1562 16.25 8.125C16.25 3.63672 12.6133 0 8.125 0C3.63672 0 0 3.63672 0 8.125C0 12.6133 3.63672 16.25 8.125 16.25C10.1562 16.25 12.0117 15.5039 13.4375 14.2734V14.7891C13.4375 14.9141 13.4883 15.0312 13.5742 15.1211L18.3164 19.8633C18.5 20.0469 18.7969 20.0469 18.9805 19.8633L19.8633 18.9805C20.0469 18.7969 20.0469 18.5 19.8633 18.3164ZM8.125 14.375C4.67188 14.375 1.875 11.5781 1.875 8.125C1.875 4.67188 4.67188 1.875 8.125 1.875C11.5781 1.875 14.375 4.67188 14.375 8.125C14.375 11.5781 11.5781 14.375 8.125 14.375Z"></path></svg>
         </div>
-        <div class="h-full w-full pr-1">
+        <div class="h-full w-full pr-2">
           <input type="search" name="search" @keyup="search" @search="search" v-model="query" placeholder="Search..."
-                 class="bg-transparent h-full outline-none placeholder-gray-500 text-gray-700 text-sm w-64">
+                 class="bg-transparent h-full outline-none placeholder-gray-500 text-gray-700 text-sm w-64 pl-1">
         </div>
       </div>
 
       <!-- New resource button -->
-      <router-link :to="{ name: 'resource-create', params: { resourceName: $route.params.resourceName } }"
-                   class="bg-blue-600 flex focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 h-full items-center px-3 rounded">
+      <router-link
+          :to="{ name: 'resource-create', params: { resourceName: $route.params.resourceName } }"
+          class="bg-blue-600 flex focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 h-full items-center px-3 rounded">
         <span class="text-base text-white">New {{ resources.labels.singular }}</span>
       </router-link>
     </div>
