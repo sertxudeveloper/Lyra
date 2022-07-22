9aa7912f008d96e828aea12ef0a30fb360ff45d4
Sergio Peris <dev.sertxu@gmail.com>
2022-07-23 00:00:27 +0200

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+        './resources/views/**/*.{html,php}',
+        './resources/js/**/*.{vue,js}',
+    ],
+    safelist: [
+        'text-left',
+        'text-center',
+        'text-right',
+        'text-justify',
+    ],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
