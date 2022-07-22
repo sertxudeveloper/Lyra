d97489a1d2bf6dfeb9e2100c293cc9f540c1ad60
Sergio Peris <dev.sertxu@gmail.com>
2022-07-23 00:15:07 +0200

    wip

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -4,6 +4,7 @@ namespace SertxuDeveloper\Lyra\Tests\Models;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use SertxuDeveloper\Lyra\Tests\Database\Factories\UserFactory;
