eab3b75af9a3f867bb2acca7564b419a9a6c3995
Sergio Peris <dev.sertxu@gmail.com>
2022-07-31 18:56:19 +0200

    wip

---
+++ resources/js/components/Fields/Detail/Markdown.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-span-3">
+    <label class="block text-sm font-medium text-gray-600">{{ field.name }}</label>
+    <div v-if="field.value" class="flex mt-1 sm:text-sm text-gray-700 w-full">
+      <span class="whitespace-no-wrap">{{ field.value }}</span>
+    </div>
+    <p v-else class="text-gray-300">&mdash;</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Body",
+  props: ['field'],
+}
+</script>
---
+++ resources/js/components/Fields/Form/Markdown.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-span-3 xl:col-span-2">
+    <label class="block text-sm font-medium text-gray-600">{{ field.name }}</label>
+    <div class="flex mt-1 rounded-md shadow-sm">
+<!--      <textarea class="block border flex-1 focus:border-blue-500 focus:ring-blue-500 outline-none pl-3 pr-2 py-2 rounded-md sm:text-sm w-full text-gray-700 min-h-[118px]"
+                :class="[ errors?.length ? errorClass : defaultClass ]"
+                rows="5" v-model.lazy="field.value" :placeholder="field.placeholer"></textarea>-->
+
+      <div ref="editor" class="border rounded-md"
+           :class="[ errors?.length ? errorClass : defaultClass ]">{{ field.value }}</div>
+    </div>
+    <div class="mt-1 px-1 text-red-500 text-xs" v-for="error in errors">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+import MarkdownEditor from '@sertxudeveloper/markdown-editor'
+
+export default {
+  name: "Textarea",
+  props: ['field', 'errors'],
+  data() {
+    return {
+      defaultClass: 'border-gray-300',
+      errorClass: 'border-red-400',
+    }
+  },
+  mounted() {
+    new MarkdownEditor(this.$refs.editor, {
+      key: this.field.key,
+      language: window.config.lang,
+      placeholder: this.field.placeholder,
+    })
+  },
+}
+</script>
